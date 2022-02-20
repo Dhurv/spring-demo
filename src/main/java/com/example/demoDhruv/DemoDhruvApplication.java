f68ce64229856752f5b7df842e95aed19a833f71
Dhruba Jyoti Bharali <official.dhruvb@gmail.com>
2022-02-20 09:46:42 +0530

    Pushing all the files to the repo

---
+++ src/main/java/com/example/demoDhruv/DemoDhruvApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoDhruv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDhruvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDhruvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoDhruv/DemoDhruvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoDhruv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDhruvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
